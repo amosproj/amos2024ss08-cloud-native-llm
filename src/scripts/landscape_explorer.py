0d28de6fb33b0f39ea1191288b1adaa45d884aef
julioc-p <14-10820@usb.ve>
2024-04-26 01:25:28 +0200

    method to create augmented yaml with download urls of files with desired extension, using sql lite to cache requests
    Signed-off-by: julioc-p <14-10820@usb.ve>

--- src/scripts/landscape_explorer.py
+++ src/scripts/landscape_explorer.py
@@ -1,14 +1,19 @@
 from collections import defaultdict
+import sys
 import requests
 import os
-import sys
+import yaml
+from tqdm import tqdm
+import requests_cache
+
 
 TOKEN = os.environ['GITHUBTOKEN']
-HEADERS = {'Authorization': f'{TOKEN}',
+HEADERS = {'Authorization': f'Bearer {TOKEN}',
            'Accept': 'application/vnd.github+json', 'X-GitHub-Api-Version': '2022-11-28'}
 BASE_API_URL = 'https://api.github.com'
 BASE_REPO_YAML = 'https://raw.githubusercontent.com/cncf/landscape/master/landscape.yml'
 EXTENSIONS = ["yml", "yaml", "pdf", "md"]
+requests_cache.install_cache('landscape_cache', expire_after=3600)
 
 
 def get_urls(repo_url: str) -> dict:
@@ -30,7 +35,9 @@ def get_urls(repo_url: str) -> dict:
     # if response.get('truncated'):
     # TODO: Handle case where response is truncated (more than 100000 registers)
     #
-    tree = response.get('tree')
+    tree = response.get('tree')  # TODO: Handle case where tree is null
+    if not tree:
+        return {}
     # select of type blob and if the extension is in the list
     res = defaultdict(list)
     base_download_url = f'https://raw.githubusercontent.com/{repo_url.split("https://github.com/")[1]}/{default_branch}/'
@@ -51,9 +58,37 @@ def get_default_branch(repo_url):
     except requests.exceptions.RequestException as e:
         print(e)
 
+    if response.status_code != 200:
+        print(f'Error: {response.status_code}')
+        return None
+
     return response.json().get('default_branch')
 
 
-if __name__ == '__main__':
+def get_augmented_yml_with_urls():
+    response = requests.get(BASE_REPO_YAML)
 
-    get_urls(sys.argv[1])
+    content = response.content.decode('utf-8')
+    content = yaml.safe_load(content)  # type dict
+    number_files = 5
+    os.makedirs('sources', exist_ok=True)
+    for category in tqdm(content.get('landscape')):
+        for subcategory in tqdm(category.get('subcategories')):
+            for item in tqdm(subcategory.get('items')):
+                if 'repo_url' not in item:
+                    continue
+                urls = get_urls(item.get('repo_url'))
+                item['download_urls'] = {}
+                for ext, url_list in urls.items():
+                    item['download_urls'][ext] = url_list
+                number_files -= 1
+                if not number_files:
+                    with open('sources/landscape_augmented.yml', 'w+') as file:
+                        yaml.dump(content, file, sort_keys=False)
+                    sys.exit(0)
+    with open('../sources/landscape_augmented.yml', 'w+') as file:
+        yaml.dump(content, file, sort_keys=False)
+
+
+if __name__ == '__main__':
+    get_augmented_yml_with_urls()
