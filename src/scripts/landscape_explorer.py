0c9f6f1d4a9576fa7467e21e7011e999139c5981
julioc-p <14-10820@usb.ve>
2024-04-25 17:02:06 +0200

    Method to get all download urls from a repo given a list of desired extensions
    Signed-off-by: julioc-p <14-10820@usb.ve>

--- src/scripts/landscape_explorer.py
+++ src/scripts/landscape_explorer.py
@@ -1,27 +1,49 @@
+from collections import defaultdict
 import requests
-import json
 import os
 import sys
 
 TOKEN = os.environ['GITHUBTOKEN']
 HEADERS = {'Authorization': f'{TOKEN}',
            'Accept': 'application/vnd.github+json', 'X-GitHub-Api-Version': '2022-11-28'}
-BASE_URL = 'https://api.github.com'
+BASE_API_URL = 'https://api.github.com'
+BASE_REPO_YAML = 'https://raw.githubusercontent.com/cncf/landscape/master/landscape.yml'
+EXTENSIONS = ["yml", "yaml", "pdf", "md"]
 
 
-def get_urls(extensions, repo_url):
+def get_urls(repo_url: str) -> dict:
+    """
+    Retrieves the URLs of files with specified extensions from a GitHub repository.
 
+    Args:
+        extensions (list): A list of file extensions to filter the URLs.
+        repo_url (str): The URL of the GitHub repository.
+
+    Returns:
+        dict: A dictionary where the keys are the file extensions and the values are lists of URLs.
+
+    """
     default_branch = get_default_branch(repo_url)
     # get tree response
-    url = f'{BASE_URL}/repos/{repo_url.split("https://github.com/")[1]}/git/trees/{default_branch}?recursive=1'
-    print(url)
-    response = requests.get(url, headers=HEADERS)
-    print(response.json())
+    url = f'{BASE_API_URL}/repos/{repo_url.split("https://github.com/")[1]}/git/trees/{default_branch}?recursive=1'
+    response = requests.get(url, headers=HEADERS).json()
+    # if response.get('truncated'):
+    # TODO: Handle case where response is truncated (more than 100000 registers)
+    #
+    tree = response.get('tree')
+    # select of type blob and if the extension is in the list
+    res = defaultdict(list)
+    base_download_url = f'https://raw.githubusercontent.com/{repo_url.split("https://github.com/")[1]}/{default_branch}/'
+    for file in tree:
+        ext = file.get('path').split('.')[-1]
+        if file.get('type') == 'blob' and ext in EXTENSIONS:
+            res[ext].append(base_download_url + file.get('path'))
+    return res
 
 
 def get_default_branch(repo_url):
 
-    url = f'{BASE_URL}/repos/{repo_url.split("https://github.com/")[1]}'
+    url = f'{BASE_API_URL}/repos/{repo_url.split("https://github.com/")[1]}'
 
     try:
         response = requests.get(
@@ -33,4 +55,5 @@ def get_default_branch(repo_url):
 
 
 if __name__ == '__main__':
-    get_urls(sys.argv[1], sys.argv[2])
+
+    get_urls(sys.argv[1])
