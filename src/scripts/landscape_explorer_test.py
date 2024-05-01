58fd4f6576a023cd483181ac80dcc04fe40d8dc4
julioc-p <14-10820@usb.ve>
2024-05-01 19:47:34 +0200

    testing nested trees if response gets truncated because of file number limit by tree
    Signed-off-by: julioc-p <14-10820@usb.ve>

--- src/scripts/landscape_explorer_test.py
+++ src/scripts/landscape_explorer_test.py
@@ -1,9 +1,8 @@
-import unittest
-from unittest.mock import Mock
-
-from landscape_explorer import get_urls
 import landscape_explorer
+import unittest
 import mock
+import landscape_explorer
+from unittest.mock import Mock
 
 
 def mocked_requests_get(*args, **kwargs):
@@ -15,17 +14,50 @@ def mocked_requests_get(*args, **kwargs):
 
         def json(self):
             return self.json_data
-
-    if args[0] == 'https://api.github.com/repos/org/repo_good/git/trees/main?recursive=1':
-        return MockResponse({
-            "tree": [
-                {"path": "file1.yml", "type": "blob"},
-                {"path": "file2.md", "type": "blob"},
-            ]
-        }, 200)
-    elif args[0] == 'http://someotherurl.com/anothertest.json':
-        return MockResponse({"key2": "value2"}, 200)
-
+    match args[0]:
+        case 'https://api.github.com/repos/org/repo_good/git/trees/main?recursive=1':
+            return MockResponse({
+                "tree": [
+                    {"path": "file1.yml", "type": "blob"},
+                    {"path": "file2.md", "type": "blob"},
+                ]
+            }, 200)
+        case 'https://api.github.com/repos/org/repo_nested/git/trees/main?recursive=1':
+            return MockResponse({
+                "tree": [
+                    {"path": "file1.yml", "type": "blob"},
+                    {"path": "tree_path1", "type": "tree", "sha": "tree_sha1"}
+                ],
+                "truncated": "true"
+            }, 200)
+        case 'https://api.github.com/repos/org/repo_nested/git/trees/main':
+            return MockResponse({
+                "tree": [
+                    {"path": "file1.yml", "type": "blob"},
+                    {"path": "file2.md", "type": "blob"},
+                    {"path": "tree_path1", "type": "tree", "sha": "tree_sha1"}
+                ]
+            }, 200)
+        case 'https://api.github.com/repos/org/repo_nested/git/trees/tree_sha1?recursive=1':
+            return MockResponse({
+                "tree": [
+                    {"path": "file3.yml", "type": "blob"},
+                    {"path": "tree_path2", "type": "tree", "sha": "tree_sha2"},
+                ],
+                "truncated": "true"}, 200)
+        case 'https://api.github.com/repos/org/repo_nested/git/trees/tree_sha1':
+            return MockResponse({
+                "tree": [
+                    {"path": "file3.yml", "type": "blob"},
+                    {"path": "file4.md", "type": "blob"},
+                    {"path": "tree_path2", "type": "tree", "sha": "tree_sha2"},
+                ]}, 200)
+        case 'https://api.github.com/repos/org/repo_nested/git/trees/tree_sha2?recursive=1':
+            return MockResponse({
+                "tree": [
+                    {"path": "file5.yml", "type": "blob"},
+                    {"path": "file6.md", "type": "blob"},
+                ]}, 200)
     return MockResponse(None, 404)
 
 
@@ -36,12 +68,32 @@ class LandscapeExplorerTest(unittest.TestCase):
 
         landscape_explorer.get_default_branch = Mock(return_value="main")
 
-        result = get_urls("https://github.com/org/repo_good")
+        result = landscape_explorer.get_urls(
+            "https://github.com/org/repo_good")
 
         expected_result = {
             "yml": ["https://raw.githubusercontent.com/org/repo_good/main/file1.yml"],
             "md": ["https://raw.githubusercontent.com/org/repo_good/main/file2.md"]
         }
+        self.assertEqual(result, expected_result)
+
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_get_urls_with_nested_files(self, mock_get):
+
+        landscape_explorer.get_default_branch = Mock(return_value="main")
+
+        result = landscape_explorer.get_urls(
+            "https://github.com/org/repo_nested")
+
+        expected_result = {
+            "yml": ["https://raw.githubusercontent.com/org/repo_nested/main/file1.yml",
+                    "https://raw.githubusercontent.com/org/repo_nested/main/tree_path1/file3.yml",
+                    "https://raw.githubusercontent.com/org/repo_nested/main/tree_path1/tree_path2/file5.yml"],
+            "md": ["https://raw.githubusercontent.com/org/repo_nested/main/file2.md",
+                   "https://raw.githubusercontent.com/org/repo_nested/main/tree_path1/file4.md",
+                   "https://raw.githubusercontent.com/org/repo_nested/main/tree_path1/tree_path2/file6.md"]
+        }
+
         self.assertEqual(result, expected_result)
 
 
