da9bcfc78f0d04aa8d1d00aa3fdab2c59780d4fc
Christian Wielenberg <christian.wielenberg@st.ovgu.de>
2024-05-17 22:13:57 +0200

    First draft of qa_generation
    Signed-off-by: Christian Wielenberg <christian.wielenberg@st.ovgu.de>

---
+++ src/scripts/qa_generation/qa_generation.py
@@ -0,0 +1,20 @@
+from datasets import load_dataset
+from lmqg import TransformersQG
+
+model = TransformersQG(language= "en", model = "lmqg/t5-base-squad-qag")
+dataset = load_dataset("Kubermatic/cncf-raw-data-for-llm-training", split = "train")
+with open("questions.csv", "w") as f:
+    f.write("Question; Answer")
+for date in dataset:
+    for chunk in date["content"]:
+        context = f"Project: {date['tag']['project_name']}\n{chunk['heading']}\n{chunk['data']}"
+        context = context[:512]
+        print(context)
+        qa = model.generate_qa(context)
+        with open("questions.csv", "a") as f:
+            for question, answer in qa:
+                f.write(f"{question}; {answer}")
+        print(qa)
+        
+
+
