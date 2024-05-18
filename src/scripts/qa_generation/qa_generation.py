41e13d861b2ef99d8a9c0adc87bd0243cc16b186
Christian Wielenberg <christian.wielenberg@st.ovgu.de>
2024-05-18 09:13:48 +0200

    updated max_length

--- src/scripts/qa_generation/qa_generation.py
+++ src/scripts/qa_generation/qa_generation.py
@@ -4,17 +4,15 @@ from lmqg import TransformersQG
 model = TransformersQG(language= "en", model = "lmqg/t5-base-squad-qag")
 dataset = load_dataset("Kubermatic/cncf-raw-data-for-llm-training", split = "train")
 with open("questions.csv", "w") as f:
-    f.write("Question; Answer\n")
+    f.write("Question; Answer; Project\n")
 for date in dataset:
     for chunk in date["content"]:
         context = f"Project: {date['tag']['project_name']}\n{chunk['heading']}\n{chunk['data']}"
-        context = context[:512]
+        context = context[:1024]
         print(context)
         qa = model.generate_qa(context)
         with open("questions.csv", "a") as f:
             for question, answer in qa:
-                f.write(f"{question}; {answer}\n")
+                f.write(f"{question}; {answer}; {date['tag']['project_name']}\n")
         print(qa)
-        
-
 
