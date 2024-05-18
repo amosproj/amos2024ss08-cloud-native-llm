85a08693589918a059e23a59d6fe35cf44111f46
julioc-p <14-10820@usb.ve>
2024-05-18 15:55:01 +0200

    using a queue of models
    Signed-off-by: julioc-p <14-10820@usb.ve>

--- src/scripts/qa_generation/qa_generation.py
+++ src/scripts/qa_generation/qa_generation.py
@@ -2,35 +2,41 @@ from datasets import load_dataset
 from lmqg import TransformersQG
 import threading
 
+from pyparsing import deque
 
-MAX_THREADS = 32
+
+MAX_THREADS = 4
+q = deque()
 
 
-def process_chunk(date, chunk):
-    semaphore.acquire()
-    context = f"Project: {date['tag']['project_name']}\n{chunk['heading']}\n{chunk['data']}"
-    context = context[:1024]
-    # print(context)
-    model = TransformersQG(language="en", model="lmqg/t5-base-squad-qag")
-    qa = model.generate_qa(context)
-    with open("questions.csv", "a") as f:
-        for question, answer in qa:
-            f.write(
-                f"{question}; {answer}; {date['tag']['project_name']}\n")
-    print(qa)
-    semaphore.release()
+def process_chunk(date, chunk, semaphore):
+    with semaphore:
+        context = f"Project: {date['tag']['project_name']}\n{chunk['heading']}\n{chunk['data']}"
+        context = context[:1024]
+        model = q.popleft()
+        qa = model.generate_qa(context)
+        with open("questions.csv", "a") as f:
+            for question, answer in qa:
+                f.write(
+                    f"{question}; {answer}; {date['tag']['project_name']}\n")
+        print(qa)
+        q.append(model)
 
 
 dataset = load_dataset(
-    data_files='Kubermatic/cncf-raw-data-for-llm-training', split="train")
+    "Kubermatic/cncf-raw-data-for-llm-training", split="train")
 with open("questions.csv", "w") as f:
     f.write("Question; Answer; Project\n")
 
+
+for i in range(MAX_THREADS):
+    q.append(TransformersQG(language="en", model="lmqg/t5-base-squad-qag"))
 for date in dataset:
     semaphore = threading.Semaphore(MAX_THREADS)
     threads = []
     for chunk in date["content"]:
-        thread = threading.Thread(target=process_chunk, args=(date, chunk))
+        thread = threading.Thread(
+            target=process_chunk, args=(date, chunk, semaphore))
 
         threads.append(thread)
         thread.start()
