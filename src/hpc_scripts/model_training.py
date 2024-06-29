3ec47393cb8c966d23ddf9d81374087acd61563a
julioc-p <14-10820@usb.ve>
2024-06-29 21:34:07 +0200

    introducing early stopping
    Signed-off-by: julioc-p <14-10820@usb.ve>

--- src/hpc_scripts/model_training.py
+++ src/hpc_scripts/model_training.py
@@ -1,31 +1,23 @@
+from transformers import (AutoModelForCausalLM,
+                          AutoTokenizer,
+                          BitsAndBytesConfig,
+                          TrainingArguments,
+                          EarlyStoppingCallback
+                          )
+from trl import SFTTrainer
+from peft import LoraConfig
+import torch
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from huggingface_hub import HfApi, login
 import os
 HF_TOKEN = os.getenv('HF_TOKEN', 'add_hf_token')
 api = HfApi()
 login(HF_TOKEN, add_to_git_credential=True)
 
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import transformers
-from datasets import load_dataset
-import torch
-import re
-from tqdm import tqdm
-import pandas as pd
-from datasets import Dataset
-from peft import LoraConfig, PeftConfig
-import bitsandbytes as bnb
-import accelerate
-from trl import SFTTrainer
-from transformers import (AutoModelForCausalLM,
-                          AutoModelForQuestionAnswering,
-                          AutoTokenizer,
-                          BitsAndBytesConfig,
-                          TrainingArguments,
-                          )
-
 
 # training pipeline taken from https://huggingface.co/blog/gemma-peft
-model_id = "google/gemma-1.1-7b-it"
+model_id = "google/gemma-2-27b-it"
 
 bnb_config = BitsAndBytesConfig(
     load_in_8bit=True,
@@ -33,12 +25,14 @@ bnb_config = BitsAndBytesConfig(
     bnb_8bit_compute_dtype=torch.bfloat16
 )
 
-tokenizer = AutoTokenizer.from_pretrained(model_id, padding_side = 'right')
+tokenizer = AutoTokenizer.from_pretrained(model_id, padding_side='right')
 # TODO: Check if this can be changed to AutoModelForQuestionAnswering with GEMMA
-model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config, device_map="auto")
+model = AutoModelForCausalLM.from_pretrained(
+    model_id, quantization_config=bnb_config, device_map="auto")
 
 # Training Data
-dataset = load_dataset("Kubermatic/cncf-question-and-answer-dataset-for-llm-training", split="train")
+dataset = load_dataset(
+    "Kubermatic/cncf-question-and-answer-dataset-for-llm-training", split="train")
 
 
 # Training (hyper)parameters (initial config taken from: https://medium.com/@lucamassaron/sherlock-holmes-q-a-enhanced-with-gemma-2b-it-fine-tuning-2907b06d2645)
@@ -68,6 +62,7 @@ training_arguments = TrainingArguments(
     lr_scheduler_type="cosine",
     report_to="tensorboard",
     disable_tqdm=False,
+    load_best_model_at_end=True,
     # debug="underflow_overflow"
 )
 
@@ -78,6 +73,7 @@ inputs = tokenizer(text, return_tensors="pt").to(device)
 outputs = model.generate(**inputs, max_new_tokens=500)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 
+
 def formatting_func(example):
     output_texts = []
     for i in range(len(example['Question'])):
@@ -85,12 +81,14 @@ def formatting_func(example):
         output_texts.append(text)
     return output_texts
 
+
 lora_config = LoraConfig(
-    lora_alpha=16,
-    lora_dropout=0,
-    r=64,
+    lora_alpha=64,
+    lora_dropout=0.1,
+    r=32,
     bias="none",
-    target_modules=["q_proj", "o_proj", "k_proj", "v_proj", "gate_proj", "up_proj", "down_proj"],
+    target_modules=["q_proj", "o_proj", "k_proj",
+                    "v_proj", "gate_proj", "up_proj", "down_proj"],
     # TODO: Check if this can be changed to QUESTION_ANS with GEMMA
     task_type="CAUSAL_LM",
 )
@@ -104,6 +102,7 @@ trainer = SFTTrainer(
     formatting_func=formatting_func,
     tokenizer=tokenizer,
     max_seq_length=max_seq_length,
+    callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
 )
 trainer.train()
 print("Model is trained")
