090b90019a93a783893c7cd2f1e740eaaf31145e
Anosh Rezaei <ahmadanosh.ar@gmail.com>
2024-07-10 09:49:00 +0200

    Final vlaues
    Signed-off-by: Anosh Rezaei <ahmadanosh.ar@gmail.com>

--- src/scripts/GUI/preparation_scripts.sh
+++ src/scripts/GUI/preparation_scripts.sh
@@ -10,7 +10,11 @@ systemctl restart docker
 docker run --gpus all nvidia/cuda:11.5.2-base-ubuntu20.04 nvidia-smi # checks if NVIDIA Container Toolkit is installed
 
 
-docker run  -it  --gpus all nvidia/cuda:12.5.0-base-ubuntu22.04 bash
-docker run  -p 8080:8080  --gpus all  --name local-ai  -ti localai/localai:latest-aio-gpu-nvidia-cuda-12
+docker run -it --gpus all nvidia/cuda:12.5.0-base-ubuntu22.04 bash
+docker run -p 8080:8080  --gpus all  --name local-ai  -ti localai/localai:latest-aio-gpu-nvidia-cuda-12
 
-docker run  -p 8080:8080  --gpus all localai/localai:v2.18.1-cublas-cuda12-ffmpeg-core https://github.com/amosproj/amos2024ss08-cloud-native-llm/blob/05b3eff175d9985bef61cf16fd7ba20831465e4a/src/scripts/GUI/model_configuration.yaml
+docker run -p 8080:8080 --gpus all --env-file .env localai/localai:v2.18.1-cublas-cuda12-ffmpeg-core https://raw.githubusercontent.com/amosproj/amos2024ss08-cloud-native-llm/110-implement-the-chat-bot-user-interface/src/scripts/GUI/model_configuration.yaml
+
+# Run localai with model config from gist
+
+docker run -p 8080:8080 --gpus all --env-file .env localai/localai:v2.18.1-cublas-cuda12-ffmpeg-core https://gist.githubusercontent.com/anosh-ar/91658012cccb8f74abb72ddc78bb71c8/raw/e00cca94739213ebf83e9074e3e9e3f74e55d7fb/model_config.yaml
